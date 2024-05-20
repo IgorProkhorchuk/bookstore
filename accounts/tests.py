81e2a66d3dbdeb4efa6112aa2ef26b5f6e3ffb28
Igor <prokhorchuk.igor@gmail.com>
2024-05-20 17:38:07 +0300

    feat: add signup page & functionality

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from .views import SignUpPageView
+from django.urls import reverse, resolve
+from .forms import CustomUserCreationForm
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
@@ -27,3 +30,40 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+    def test_signup_user(self):
+        form_data = {
+            'username': 'ivan',
+            'email': 'ivan@mail.com',
+            'password1': 'testpass',
+        }
+        request = self.client.post('/accounts/signup/', form_data)
+        self.assertEqual(request.status_code, 200)
+
+        def test_user_created_succesfully(User):
+            user = User.objects.get(username='ivan')
+            self.assertEqual(user.email, 'ivan@mail.com')
+
+class SignupPageTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(
+            view.func.__name__,
+            SignUpPageView.as_view().__name__
+        )
+
