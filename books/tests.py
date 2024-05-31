369ab6ca8e95814fd6327735b4fac02313841e44
Igor Prokhorchuk <igor@prokhorchuk.com>
2024-05-31 22:39:54 +0200

    fix: add tests to reviews model

--- books/tests.py
+++ books/tests.py
@@ -1,14 +1,28 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import Book
+from .models import Book, Review
+from django.contrib.auth import get_user_model
 
 class BookTests(TestCase):
 
     def setUp(self):
+
+        self.user = get_user_model().objects.create_user(
+            username = 'User1',
+            email = 'user1@mail.com',
+            password = 'password123',
+        )
+
         self.book = Book.objects.create(
             title = "Book title",
             author = "Book author",
             price = '25.00',
+        )
+
+        self.review = Review.objects.create(
+            book = self.book,
+            author = self.user,
+            review = 'Such a book!!',
         )
 
     def test_book_listing(self):
@@ -28,5 +42,7 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Book title')
+        self.assertContains(response, 'Such a book!!')
         self.assertTemplateUsed(response, 'books/book_detail.html')
+
 
